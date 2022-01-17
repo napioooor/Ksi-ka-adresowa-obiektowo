2b1a6d4437b419de60d9b2102822a1d6761d7f6b
Jan Napiórkowski <jasiek19975@gmail.com>
2022-01-18 00:08:44 +0100

    Dodano funkcję dodawania użytkowników do pliku

--- KsiazkaAdresowa.cpp
+++ KsiazkaAdresowa.cpp
@@ -1,10 +1,14 @@
 #include "KsiazkaAdresowa.h"
 
+KsiazkaAdresowa::KsiazkaAdresowa() {
+    nazwaPlikuZUzytkownikami = "Uzytkownicy.txt";
+}
+
 void KsiazkaAdresowa::rejestracjaUzytkownika() {
     Uzytkownik uzytkownik = podajDaneNowegoUzytkownika();
 
     uzytkownicy.push_back(uzytkownik);
-    //dopiszUzytkownikaDoPliku(uzytkownik);
+    dopiszUzytkownikaDoPliku(uzytkownik);
 
     cout << endl << "Konto zalozono pomyslnie" << endl << endl;
     system("pause");
@@ -16,7 +20,7 @@ Uzytkownik KsiazkaAdresowa::podajDaneNowegoUzytkownika() {
     uzytkownik.ustawId(pobierzIdNowegoUzytkownika());
 
     do {
-        cout << endl << "Podaj login: ";
+        cout << "Podaj login: ";
         uzytkownik.ustawLogin(wczytajLinie());
     } while (czyIstniejeLogin(uzytkownik.pobierzLogin()) == true);
 
@@ -34,8 +38,8 @@ int KsiazkaAdresowa::pobierzIdNowegoUzytkownika() {
 }
 
 bool KsiazkaAdresowa::czyIstniejeLogin(string login) {
-    for(int i = 0; i < uzytkownicy.size(); i++){
-        if(uzytkownicy[i].pobierzLogin() == login){
+    for(int i = 0; i < uzytkownicy.size(); i++) {
+        if(uzytkownicy[i].pobierzLogin() == login) {
             cout << endl << "Istnieje uzytkownik o takim loginie." << endl;
             return true;
         }
@@ -43,9 +47,51 @@ bool KsiazkaAdresowa::czyIstniejeLogin(string login) {
     return false;
 }
 
-string KsiazkaAdresowa::wczytajLinie()
-{
+string KsiazkaAdresowa::wczytajLinie() {
     string wejscie = "";
     getline(cin, wejscie);
     return wejscie;
+}
+
+void KsiazkaAdresowa::dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik) {
+    fstream plikTekstowy;
+    string liniaZDanymiUzytkownika = "";
+    plikTekstowy.open(nazwaPlikuZUzytkownikami.c_str(), ios::app);
+
+    if (plikTekstowy.good() == true) {
+        liniaZDanymiUzytkownika = zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(uzytkownik);
+
+        if (czyPlikJestPusty(plikTekstowy) == true) {
+            plikTekstowy << liniaZDanymiUzytkownika;
+        } else {
+            plikTekstowy << endl << liniaZDanymiUzytkownika ;
+        }
+    } else
+        cout << "Nie udalo sie otworzyc pliku " << nazwaPlikuZUzytkownikami << " i zapisac w nim danych." << endl;
+    plikTekstowy.close();
+}
+
+string KsiazkaAdresowa::zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik) {
+    string liniaZDanymiUzytkownika = "";
+
+    liniaZDanymiUzytkownika += konwerjsaIntNaString(uzytkownik.pobierzId())+ '|';
+    liniaZDanymiUzytkownika += uzytkownik.pobierzLogin() + '|';
+    liniaZDanymiUzytkownika += uzytkownik.pobierzHaslo() + '|';
+
+    return liniaZDanymiUzytkownika;
+}
+
+string KsiazkaAdresowa::konwerjsaIntNaString(int liczba) {
+    ostringstream ss;
+    ss << liczba;
+    string str = ss.str();
+    return str;
+}
+
+bool KsiazkaAdresowa::czyPlikJestPusty(fstream &plikTekstowy) {
+    plikTekstowy.seekg(0, ios::end);
+    if (plikTekstowy.tellg() == 0)
+        return true;
+    else
+        return false;
 }
